2e2044b2b4e49c1e0135227870ab68409aa1bd1a
lezzen <yanezbruce123@gmail.com>
2023-08-29 21:44:27 -0500

    added functionality to clear and search buttons

--- script.js
+++ script.js
@@ -6,79 +6,101 @@ let meteoriteData; // The code below saves the result of the API call to this va
 // The available keys are: name, id, nametype, recclass, mass, fall, year, reclat, reclong, geolocation.
 
 async function fetchMeteoriteData() {
-    try {
-        const response = await fetch("https://data.nasa.gov/resource/gh4g-9sfh.json");
-        if (!response.ok) throw new Error("Connection to API unsuccessful");
-        const data = await response.json();
-        return data; // This function returns a promise
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await fetch(
+      "https://data.nasa.gov/resource/gh4g-9sfh.json"
+    );
+    if (!response.ok) throw new Error("Connection to API unsuccessful");
+    const data = await response.json();
+    return data; // This function returns a promise
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-fetchMeteoriteData().then(response => meteoriteData = response); // This calls the function that fetches the API data and then assigns it to the variable
+fetchMeteoriteData().then((response) => (meteoriteData = response)); // This calls the function that fetches the API data and then assigns it to the variable
+//code below is for the search and clear buttons
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const year = document.getElementById("year").value;
+  const recclass = document.getElementById("recclass").value;
+  const massRangeInputs = document.querySelectorAll(
+    ".form_control_container__time__input"
+  );
+
+  //has all the inputs values
+  const parameters = {
+    name,
+    year,
+    recclass,
+    massRangeMin: massRangeInputs[0].value,
+    massRangeMax: massRangeInputs[1].value,
+  };
+});
 
 // The code below is for the Mass Range slider
 
 function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
-    const [from, to] = getParsed(fromInput, toInput);
-    fillSlider(fromInput, toInput, "#C6C6C6", "#25daa5", controlSlider);
-    if (from > to) {
-      fromSlider.value = to;
-      fromInput.value = to;
-    } else {
-      fromSlider.value = from;
-    }
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, "#C6C6C6", "#25daa5", controlSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromSlider.value = from;
   }
-  
-  function controlToInput(toSlider, fromInput, toInput, controlSlider) {
-    const [from, to] = getParsed(fromInput, toInput);
-    fillSlider(fromInput, toInput, "#C6C6C6", "#25daa5", controlSlider);
-    setToggleAccessible(toInput);
-    if (from <= to) {
-      toSlider.value = to;
-      toInput.value = to;
-    } else {
-      toInput.value = from;
-    }
+}
+
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, "#C6C6C6", "#25daa5", controlSlider);
+  setToggleAccessible(toInput);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
   }
-  
-  function controlFromSlider(fromSlider, toSlider, fromInput) {
-    const [from, to] = getParsed(fromSlider, toSlider);
-    fillSlider(fromSlider, toSlider, "#C6C6C6", "#25daa5", toSlider);
-    if (from > to) {
-      fromSlider.value = to;
-      fromInput.value = to;
-    } else {
-      fromInput.value = from;
-    }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "#C6C6C6", "#25daa5", toSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromInput.value = from;
   }
-  
-  function controlToSlider(fromSlider, toSlider, toInput) {
-    const [from, to] = getParsed(fromSlider, toSlider);
-    fillSlider(fromSlider, toSlider, "#C6C6C6", "#25daa5", toSlider);
-    setToggleAccessible(toSlider);
-    if (from <= to) {
-      toSlider.value = to;
-      toInput.value = to;
-    } else {
-      toInput.value = from;
-      toSlider.value = from;
-    }
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "#C6C6C6", "#25daa5", toSlider);
+  setToggleAccessible(toSlider);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+    toSlider.value = from;
   }
-  
-  function getParsed(currentFrom, currentTo) {
-    const from = parseInt(currentFrom.value, 10);
-    const to = parseInt(currentTo.value, 10);
-    return [from, to];
-  }
-  
-  function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-    const rangeDistance = to.max - to.min;
-    const fromPosition = from.value - to.min;
-    const toPosition = to.value - to.min;
-    controlSlider.style.background = `linear-gradient(
+}
+
+function getParsed(currentFrom, currentTo) {
+  const from = parseInt(currentFrom.value, 10);
+  const to = parseInt(currentTo.value, 10);
+  return [from, to];
+}
+
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+  const rangeDistance = to.max - to.min;
+  const fromPosition = from.value - to.min;
+  const toPosition = to.value - to.min;
+  controlSlider.style.background = `linear-gradient(
         to right,
         ${sliderColor} 0%,
         ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
@@ -86,27 +108,26 @@ function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
         ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
         ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
         ${sliderColor} 100%)`;
-  }
-  
-  function setToggleAccessible(currentTarget) {
-    const toSlider = document.querySelector("#toSlider");
-    if (Number(currentTarget.value) <= 0) {
-      toSlider.style.zIndex = 2;
-    } else {
-      toSlider.style.zIndex = 0;
-    }
-  }
-  
-  const fromSlider = document.querySelector("#fromSlider");
+}
+
+function setToggleAccessible(currentTarget) {
   const toSlider = document.querySelector("#toSlider");
-  const fromInput = document.querySelector("#fromInput");
-  const toInput = document.querySelector("#toInput");
-  fillSlider(fromSlider, toSlider, "#C6C6C6", "#25daa5", toSlider);
-  setToggleAccessible(toSlider);
-  
-  fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
-  toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
-  fromInput.oninput = () =>
-    controlFromInput(fromSlider, fromInput, toInput, toSlider);
-  toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
-  
+  if (Number(currentTarget.value) <= 0) {
+    toSlider.style.zIndex = 2;
+  } else {
+    toSlider.style.zIndex = 0;
+  }
+}
+
+const fromSlider = document.querySelector("#fromSlider");
+const toSlider = document.querySelector("#toSlider");
+const fromInput = document.querySelector("#fromInput");
+const toInput = document.querySelector("#toInput");
+fillSlider(fromSlider, toSlider, "#C6C6C6", "#25daa5", toSlider);
+setToggleAccessible(toSlider);
+
+fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+fromInput.oninput = () =>
+  controlFromInput(fromSlider, fromInput, toInput, toSlider);
+toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
